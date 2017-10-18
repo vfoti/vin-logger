ca9dfad677ba37137b03e05e6ba6b142481c8807
Vincent <Vinmint@gmail.com>
2017-10-18 11:47:00 -0400

    add more logging

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+	console.log('--------------------------------');
+	console.log('authorization', atob(req.get("authorization")));
+	console.log('x-forwarded-proto', req.get('x-forwarded-proto'));
+	console.log('x-forwarded-for', req.get('x-forwarded-for'));
+	console.log('x-forwarded-port', req.get('x-forwarded-port'));
+	console.log('params', req.params);
+	console.log('query', req.query);
+	console.log('body', req.body);
+	console.log('--------------------------------');
+
+	res.send('OK');
 });
 
 app.post('/', function (req, res) {
@@ -16,12 +27,13 @@ app.post('/', function (req, res) {
 	console.log('x-forwarded-port', req.get('x-forwarded-port'));
 	console.log('params', req.params);
 	console.log('query', req.query);
+	console.log('body', req.body);
 	console.log('--------------------------------');
-	
+
 	res.send('OK');
 });
 
 // Heroku
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
 });
