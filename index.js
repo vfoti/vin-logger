2ff7210cbc1e7e2c59255c2ba0bb1963a93438c6
Vincent <Vinmint@gmail.com>
2017-10-18 11:20:39 -0400

    added Heroku port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-//app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -11,11 +11,7 @@ app.post('/', function (req, res) {
 	console.log(req);
 });
 
-/*
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// Heroku
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
 });
-*/
-
-//app.listen(process.env.PORT, process.env.IP); // Cloud 9
-app.listen(app.get('port')); // Heroku
