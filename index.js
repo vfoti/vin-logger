2434e4204648766e6a2a214f4660a305173a0e8c
Vincent <Vinmint@gmail.com>
2017-10-18 12:27:05 -0400

    added put

--- index.js
+++ index.js
@@ -33,6 +33,21 @@ app.post('/', function (req, res) {
 	res.send('OK');
 });
 
+app.put('/', function (req, res) {
+
+	console.log('--------------------------------');
+	console.log('authorization', req.get("authorization"));
+	console.log('x-forwarded-proto', req.get('x-forwarded-proto'));
+	console.log('x-forwarded-for', req.get('x-forwarded-for'));
+	console.log('x-forwarded-port', req.get('x-forwarded-port'));
+	console.log('params', req.params);
+	console.log('query', req.query);
+	console.log('body', req.body);
+	console.log('--------------------------------');
+
+	res.send('OK');
+});
+
 // Heroku
 app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
