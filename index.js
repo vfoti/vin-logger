dc1c3178fc0ba710007946971cc174ccbb6b24b9
Vincent Foti <Vincent.Foti@wakefern.com>
2019-11-20 11:05:53 -0500

    expanded middleware

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const xmlparser = require('express-xml-bodyparser');
 const app = express();
 
+app.use(bodyParser.text())
+app.use(bodyParser.json())
 app.use(bodyParser.raw())
+app.use(xmlparser())
 
 app.set('port', (process.env.PORT || 5000));
 
