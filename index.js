1cfdab22e0559e864d69b65360104db91e475b81
Vincent Foti <Vincent.Foti@wakefern.com>
2019-11-20 11:02:21 -0500

    bodyparser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.text());
+app.use(bodyParser.raw())
 
 app.set('port', (process.env.PORT || 5000));
 
