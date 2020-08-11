4ef246ddbc2bb34ac49b71ac50209dae1b54ef34
Vincent Foti <Vincent.Foti@wakefern.com>
2020-08-11 16:27:09 -0400

    adds hook for airship

--- index.js
+++ index.js
@@ -12,7 +12,23 @@ app.use(xmlparser());
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/validate', (req, res) => {
-	res.send('a8fb5f33-f68a-405c-b174-da45a9458836');
+	res.send({ 'validation_code': 'a8fb5f33-f68a-405c-b174-da45a9458836' });
+});
+
+app.get('/inbound-sms', function (req, res) {
+
+	console.log('--------------------------------GET');
+	console.log('base URL', req.baseUrl);
+	console.log('authorization', req.get("authorization"));
+	console.log('x-forwarded-proto', req.get('x-forwarded-proto'));
+	console.log('x-forwarded-for', req.get('x-forwarded-for'));
+	console.log('x-forwarded-port', req.get('x-forwarded-port'));
+	console.log('params', req.params);
+	console.log('query', req.query);
+	console.log('body', req.body);
+	console.log('--------------------------------');
+
+	res.send('OK');
 });
 
 app.get('/', function (req, res) {
